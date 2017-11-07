735116aa9ac136776e760b83a218dd191a483c8d
Julie Pan <ycp217@nyu.edu>
2017-11-07 12:18:03 -0500

    styling + swap out macbook img w/ transparent bg one

--- src/ProductsPage.js
+++ src/ProductsPage.js
@@ -1,14 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Route,
   Link
 } from 'react-router-dom';
 import OutlinedButton from './OutlinedButton';
 import FontIcon from 'material-ui/FontIcon';
+import macbookListMockup from './images/mockup_listpage_on_macbook.png';
 
 const DEFAULT_BACKGROUND_GREY = '#ECECEC';
 const LIGHTER_BACKGROUND_GREY = '#F8F8F8';
 const GREYFONT = '#E2E3DD';
+
+const SyncCommunicationsToContact = () => (
+  <div>
+    <div>
+      <h4>Flexible List Building but Have Information All Go to One Place</h4>
+    </div>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div>
+        <img height={200} src={macbookListMockup} />
+      </div>
+      <div>
+        <div>One Place to Check for All Previous Communication</div>
+        <div>
+          <p>Create as many media lists with duplicates as you want, when you update the contact in one, we'll update the same contact in all other lists that it is on.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+
+const DifferentEmailForDifferentClients = () => (
+  <div>
+    <div>Email Integrations</div>
+    <div>
+      <div>Different Email Address for Each Client</div>
+      <p>Set up your multiple email addresses with our service and attach email signature to each one for easy switching when your workon different client's campaigns</p>
+    </div>
+    <div>
+      <div>Gmail/Outlook/Private Email Server</div>
+      <p>Easily integrate our platform to use your existing email provider to deliver your campaigns</p>
+    </div>
+    <div>
+      <button>Try It for Yourself</button>
+    </div>
+  </div>
+  );
+
 
 const PersonalizeYourPitch = () => (
   <div>
@@ -57,81 +95,54 @@ const EmailAnalytics = () => (
   </div>
   );
 
+const ProductFeatureCard = ({title, tagline, to}) => (
+    <div className='horizontal-center' style={{margin: '10px 0'}} >
+      <div style={{padding: 20, background: LIGHTER_BACKGROUND_GREY}} >
+        <h4>{title}</h4>
+        <div>{tagline}</div>
+        <div style={{margin: 10}} >
+          <Link to={to} >
+            <OutlinedButton>Learn More</OutlinedButton>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+
 const ProductOverview = () => (
   <div>
-    <div className='horizontal-center'>
-      <div style={{padding: 20, background: 'lightblue'}} >
-        <h4>Flexible List-Making while Keeping Contacts Up-to-Date</h4>
-        <div>No more duplicate information across multiple Excel sheets for the same contacts. We know that building media list is your bread-and-butter, but sync-ing information up-to-date across lists shouldn't be a pain.</div>
-        <div style={{margin: 10}} >
-          <Link to='/products/organize'>
-            <OutlinedButton>Learn More</OutlinedButton>
-          </Link>
-        </div>
-      </div>
+    <div className='horizontal-center' style={{margin: '10px 0'}} >
+      <span style={{fontSize: '2.2em'}} >Tagline here meet here</span>
+    </div>
+    <div className='horizontal-center' style={{margin: '20px 0'}} >
+      <span style={{fontSize: '1.8em'}} >Organize your contacts blah blah blah taglineeeeeeee</span>
     </div>
     <div className='horizontal-center'>
-      <div style={{padding: 20, background: 'lightblue'}} >
-        <h4>PERSONALIZE YOUR PITCH</h4>
-        <div>marketing stucfffff</div>
-        <div style={{margin: 10}} >
-          <Link to='/products/personalize-pitch'>
-            <OutlinedButton>Learn More</OutlinedButton>
-          </Link>
-        </div>
-      </div>
+      <div style={{width: 800, height: 500, border: '5px solid green'}} > PRODUCT IMAGE PLACEHOLDER HERE</div>
     </div>
-    <div className='horizontal-center'>
-      <div style={{padding: 20, background: 'lightblue'}} >
-        <h4>EMAIL ANALYTICS</h4>
-        <div>marketing stucfffff</div>
-        <div style={{margin: 10}} >
-          <Link to='/products/email-analytics'>
-            <OutlinedButton>Learn More</OutlinedButton>
-          </Link>
-        </div>
-      </div>
-    </div>
-    <div className='horizontal-center'>
-      <div style={{padding: 20, background: 'lightblue'}} >
-        <h4>DIFFERENT EMAILS</h4>
-        <div>marketing stucfffff</div>
-        <div style={{margin: 10}} >
-          <Link to='/products/different-emails'>
-            <OutlinedButton>Learn More</OutlinedButton>
-          </Link>
-        </div>
-      </div>
-    </div>
+    <ProductFeatureCard
+    title='Flexible List-Making while Keeping Contacts Up-to-Date'
+    tagline="No more duplicate information across multiple Excel sheets for the same contacts. We know that building media list is your bread-and-butter, but sync-ing information up-to-date across lists shouldn't be a pain."
+    to='/products/organize'
+    />
+    <ProductFeatureCard
+    title='PERSONALIZE YOUR PITCH'
+    tagline="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    to='/products/personalize-pitch'
+    />
+    <ProductFeatureCard
+    title='EMAIL ANALYTICS'
+    tagline="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    to='/products/email-analytics'
+    />
+    <ProductFeatureCard
+    title='DIFFERENT EMAILS'
+    tagline="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    to='/products/different-emails'
+    />
   </div>
   );
 
-const DifferentEmailForDifferentClients = () => (
-  <div>
-    <div>Email Integrations</div>
-    <div>
-      <div>Different Email Address for Each Client</div>
-      <p>Set up your multiple email addresses with our service and attach email signature to each one for easy switching when your workon different client's campaigns</p>
-    </div>
-    <div>
-      <div>Gmail/Outlook/Private Email Server</div>
-      <p>Easily integrate our platform to use your existing email provider to deliver your campaigns</p>
-    </div>
-    <div>
-      <button>Try It for Yourself</button>
-    </div>
-  </div>
-  );
-
-const SyncCommunicationsToContact = () => (
-  <div>
-    <div>Flexible List Building but Have Information All Go to One Place</div>
-    <div>
-      <div>One Place to Check for All Previous Communication</div>
-      <p>Create as many media lists with duplicates as you want, when you update the contact in one, we'll update the same contact in all other lists that it is on.</p>
-    </div>
-  </div>
-  );
 
 const routes = [
   {
@@ -178,15 +189,6 @@ const ProductsPage = props => (
     padding: '50px 0',
     background: DEFAULT_BACKGROUND_GREY,
   }} >
-    <div className='horizontal-center' style={{margin: '10px 0'}} >
-      <span style={{fontSize: '2.2em'}} >Tagline here meet here</span>
-    </div>
-    <div className='horizontal-center' style={{margin: '20px 0'}} >
-      <span style={{fontSize: '1.8em'}} >Organize your contacts blah blah blah taglineeeeeeee</span>
-    </div>
-    <div className='horizontal-center'>
-      <div style={{width: 800, height: 500, border: '5px solid green'}} > PRODUCT IMAGE PLACEHOLDER HERE</div>
-    </div>
     {routes.map((route, i) =>
       <div>
         <Route exact={route.exact} key={route.path} path={route.path} component={route.component} />
